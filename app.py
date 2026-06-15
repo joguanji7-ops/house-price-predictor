33f02322ad01664faab15e7c50e5f0871ac0f646
joguanji7-ops <joguanji7@gmail.com>
2026-06-15 23:15:25 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -15,6 +15,13 @@ with open('model_columns.pkl', 'rb') as f:
 @app.route('/')
 def home():
     return render_template('index.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
